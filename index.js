7368475e3a365f2624ddb7eb00681bc2f930fcd1
Taimuraz <parabotic@gmail.com>
2017-08-31 15:32:17 +0300

    -phone number validation complete.

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 
 var MyForm = {
-	validate : function(){ alert("AAAAA")}
-	getData  : function(){ }
-	setData  : function(Object){ }
-	submit
+	validate : function(){
+	     var phone_sum_limit = 30;
+		 var isValid = true;
+		 var res_arr = [];
+		 var inv_elem_arr = [];
+		 var pattern_1 = /^\+7\(([0-9]{3})\)([0-9]{3})[-. ]([0-9]{2})[-. ]([0-9]{2})$/;
+         var pattern_2 = /\d/g ;
+
+		 var phone = document.forms["myForm"]["input_phone"].value;
+		 if( !phone.match(pattern_1)){
+		     isValid = false;
+		     inv_elem_arr.push("input_phone");
+         }else{
+             var digits = phone.match(pattern_2);
+             var sum = 0;
+             if( digits != null){
+                 for(var i = 0; i < digits.length; i ++){
+                     sum = sum + Number(digits[i]);
+                 }
+                 if(sum > phone_sum_limit){
+                     isValid = false;
+                     inv_elem_arr.push("input_phone");
+                 }
+             }
+         }
+
+
+
+		 res_arr.push(isValid);
+		 res_arr.push(inv_elem_arr);
+		return res_arr;
+	},
+	getData  : function(){ },
+	setData  : function(Object){ },
+
+
+    submit   : function(){
+	    alert(this.validate())
+    }
 };
